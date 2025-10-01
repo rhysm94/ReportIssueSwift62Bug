eced08cbc09e0fe708493ec29598be052b27aecc
Rhys Morgan <rhysm@me.com>
2025-10-01 22:54:56 +0100

    Encountered this issue when using `isolated deinit` on `TestStore` The issue is never reported, so this test actually fails, as no issue is reported in this particular condition. Using the `mainActorNow` code instead works fine though, so I suspect the issue is actually somehow in `reportIssue`

--- Tests/ReportIssueSwift62BugTests/ReportIssueSwift62BugTests.swift
+++ Tests/ReportIssueSwift62BugTests/ReportIssueSwift62BugTests.swift
@@ -1,6 +1,52 @@
+import CasePaths
+import ComposableArchitecture
 import Testing
 @testable import ReportIssueSwift62Bug
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+// Passes on `main` and fails on `remove-main-actor-now`
+@Test func receiveAction_IgnoredAction() async {
+  await withExpectedIssue {
+    let clock = TestClock()
+
+    let store = await TestStore(initialState: ReducerWithDelay.State(count: 0)) {
+      ReducerWithDelay(clock: clock)
+    }
+
+    await store.send(.incrementAfterDelay)
+    await clock.advance(by: .seconds(2))
+  }
+}
+
+struct ReducerWithDelay: Reducer, Sendable {
+  let clock: any Clock<Duration>
+
+  struct State: Equatable {
+    var count: Int
+  }
+
+  @CasePathable
+  enum Action: Equatable {
+    case incrementAfterDelay
+    case increment
+  }
+
+  init(clock: some Clock<Duration>) {
+    self.clock = clock
+  }
+
+  var body: some ReducerOf<Self> {
+    Reduce { state, action in
+      switch action {
+      case .incrementAfterDelay:
+        return .run { send in
+          try await clock.sleep(for: .seconds(1))
+          await send(.increment)
+        }
+
+      case .increment:
+        state.count += 1
+        return .none
+      }
+    }
+  }
 }
